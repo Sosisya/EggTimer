35cd7090cd7744c937fa4729a3f71b5f236721bc
Luiza <khaj-luizkin@yandex.ru>
2023-01-18 12:20:17 +0300

    Initial Commit

---
+++ Egg Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Egg Timer
+//
+//  Created by Луиза Самойленко on 18.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
