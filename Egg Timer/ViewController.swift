f232f5d7cb2956b42f989b2b63c5deb8a35943cb
Luiza <khaj-luizkin@yandex.ru>
2023-01-27 16:26:10 +0300

    End app

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -1,19 +1,58 @@
-//
-//  ViewController.swift
-//  Egg Timer
-//
-//  Created by Луиза Самойленко on 18.01.2023.
-//
-
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
+
+    @IBOutlet weak var eggLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    var secondsRemaining = 60
+    var eggCountdown: Float = 0
+    var timer = Timer()
+    let eggTimes: [String : Float] = ["Soft": 5, "Medium": 420, "Hard": 720]
+    let maxTime: Float = 720
+    var secondsOfEveryEgg: Float = 0
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func selected(_ sender: UIButton) {
 
+        timer.invalidate()
+
+        let hardness = (sender.titleLabel?.text)!
+        let secondsInEggSender = eggTimes[hardness]!
+        secondsOfEveryEgg = secondsInEggSender
+        let pecentOfEgg =  secondsOfEveryEgg / maxTime
+        eggCountdown = pecentOfEgg
+
+        switch hardness {
+        case "Soft":
+            progressBar.progress = eggCountdown
+        case "Medium":
+            progressBar.progress = eggCountdown
+        case "Hard":
+            progressBar.progress = eggCountdown
+        default:
+            break
+        }
+
+        timer =  Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if self.secondsOfEveryEgg > 0 {
+                print ("\(self.secondsOfEveryEgg) seconds.")
+                self.progressBar.progress -= self.eggCountdown / self.secondsOfEveryEgg
+                self.secondsOfEveryEgg -= 1.0
+            } else {
+                Timer.invalidate()
+                self.eggLabel.text = "Done"
+                self.playSound()
+            }
+        }
+    }
+
+    func playSound() {
+       let url = Bundle.main.url(forResource: "readySound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+    }
 }
-
